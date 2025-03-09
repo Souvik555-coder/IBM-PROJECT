5f6090292fe9bc054c2fd80eea8b1181a2dfaf4d
Souvik Bhattacharya <lulu711227@gmail.com>
2025-03-10 01:59:41 +0530

    First Commit

---
+++ DAY-10/CLASSWORK/index4-1.js
@@ -0,0 +1,34 @@
+let str = "SVU University"
+
+
+console.log(str.length)
+
+str[0]="R"
+console.log(str)
+
+let arr = [1, 2, 3, "kunal", 4, 5, true, false]
+console.log(arr[3])
+
+// arr[3] = "souvik"
+
+console.log(arr[3])
+
+console.log(arr)
+
+console.log(arr[arr.length-1])
+
+arr.push("souvik")
+
+console.log(arr)
+
+arr[4]="souvik"
+
+arr.pop()
+
+console.log(arr)
+
+const obj = {
+    kunal:{class:"B.tech",year:3,
+        
+    }
+}
